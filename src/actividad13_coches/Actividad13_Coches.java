baac5618b91b7f73a3ffa7f96a76a2bb35b27733
shakyhip <shakyhip@users.noreply.github.com>
2018-07-31 16:59:07 -0500

    Primer commit

---
+++ src/actividad13_coches/Actividad13_Coches.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad13_coches;
+
+/**
+ *
+ * @author SHAVA
+ */
+public class Actividad13_Coches {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Coche carro1 = new Coche("Nissan", "Camioneta", 200);
+        Coche carro2 = new Coche("Ferrari", "Deportivo", 100);
+        
+        final int DISTANCIA = 100; //final significa que nunca cambiará el valor
+        
+        double tiempo1 = Tiempo(DISTANCIA, carro1.getVelocidad());
+        double tiempo2 = Tiempo(DISTANCIA, carro2.getVelocidad());
+        
+        CompararTiempo(tiempo1, tiempo2);
+    }
+    
+    public static double Tiempo(int DISTANCIA,int velocidad)
+    {
+        double tiempo = (double)DISTANCIA/(double)velocidad;
+        return tiempo;
+    }
+    
+    public static void CompararTiempo(double tiempo1, double tiempo2){
+        if(tiempo1 < tiempo2)
+        {
+            System.out.println("El coche más rápido es el primer coche");
+        }
+        else if (tiempo1 == tiempo2){
+            System.out.println("Ambos son igual de rapidos");
+        }
+        else{
+            System.out.println("El coche más rápido es el segundo coche");
+        }
+    }
+}
---
+++ src/actividad13_coches/Coche.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad13_coches;
+
+/**
+ *
+ * @author SHAVA
+ */
+public class Coche {
+    private String marca;
+    private String tipo;
+    private int velocidad;
+    
+    public Coche(String marca, String tipo, int velocidad)
+    {
+        this.marca = marca;
+        this.tipo = tipo;
+        this.velocidad = velocidad;
+    }
+    
+    public void Avanzar(int n_km){
+        
+    }
+    public void Parar(){
+        
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+}
